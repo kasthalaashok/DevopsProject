883f0b20cbf748c5198917167b11ecad25928836
Admin <Admin@DESKTOP-NQSVEFH>
2023-08-10 15:30:11 +0530

    To create a class in java project

---
+++ DevopsProject/src/Com/lbrce/Devops/Test2.java
@@ -0,0 +1,11 @@
+package Com.lbrce.Devops;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Creating a new class in devops project");
+
+	}
+
+}
